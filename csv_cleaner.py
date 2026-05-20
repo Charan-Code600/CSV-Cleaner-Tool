6f341a302646699751cc2dc9a54aa5951d8582be
Charan Aade <280011215+Charan-Code600@users.noreply.github.com>
2026-05-20 10:26:52 +0530

    Refactor CSV Cleaner tool with enhanced features
    Updated the CSV Cleaner tool to include options for checking missing values, removing duplicates, and cleaning data. Enhanced user interaction with clearer prompts and options.

--- csv_cleaner.py
+++ csv_cleaner.py
@@ -1,19 +1,80 @@
-# 🧹 CSV Cleaner Tool
+import pandas as pd
 
-A Python tool to clean messy CSV files.
+print("""
+====================
+    CSV Cleaner 
+====================
+    
+    Check missing values      enter ---> 1
+    Remove duplicates         enter ---> 2
+    Clean the data            enter ---> 3
+    Close                     enter ---> 4
+      
+-----------------------------------------------
+""")
 
-## Features
-- Check missing values
-- Remove duplicate rows
-- Clean data (fill with 0, delete rows, or fill with average)
-- Save clean CSV file
+while True:
+    data1 = input("Enter file name: ")
+    
+    if data1 == "close":
+        print("✅ Closed!")
+        break
+    
+    try:
+        df = pd.read_csv(data1)
+        print(df)
+        
+        while True:
+            option = input("Choose: ")
+            
+            if option == "1":
+                print("\n=== Missing Values ===")
+                print(df.isnull().sum())
 
-## Technologies Used
-- Python
-- Pandas
+            elif option == "2":
+                print("Duplicates:", df.duplicated().sum())
+                df = df.drop_duplicates()
+                print("✅ Duplicates removed!")
 
-## How to Use
-1. Run the program
-2. Enter your CSV file name
-3. Choose cleaning option
-4. Save clean file
+            elif option == "3":
+                print("""
+    1. Fill with 0s
+    2. Delete rows
+    3. Fill with the average
+                """)
+                clean = input("Choose: ")
+                
+                if clean == "1":
+                    df = df.fillna(0)
+                    print("✅ Filled with 0s!")
+                elif clean == "2":
+                    df = df.dropna()
+                    print("✅ Rows deleted!")
+                elif clean == "3":
+                    df = df.fillna(df.mean(numeric_only=True))
+                    print("✅ Filled with average!")
+                else:
+                    print("❌ Invalid option!")
+                
+                if clean in ["1", "2", "3"]:
+                    save = input("Save file name: ")
+                    df.to_csv(save, index=False)
+                    print(f"✅ {save} saved!")
+
+            elif option == "4":
+                print("✅ Closed!")
+                break
+            
+            else:
+                print("❌ Invalid option!")
+                
+    except FileNotFoundError:
+        print("❌ File not found!")
+    
+    else:
+        break
+
+
+
+
+
